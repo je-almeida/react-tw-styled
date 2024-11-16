a8d3d504b5af66b06f17f7c5ec91ed115881a4b7
je-almeida <jeff.ifsp@gmail.com>
2024-11-15 22:33:51 -0300

    add ts file

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export declare const twStyle: (defaultClasses: string) => (classes?: string) => string;
--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,6 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.twStyle = void 0;
-var tailwind_merge_1 = require("tailwind-merge");
-var twStyle = function (defaultClasses) {
+import { twMerge } from "tailwind-merge";
+export var twStyle = function (defaultClasses) {
     return function (classes) {
-        return (0, tailwind_merge_1.twMerge)("".concat(defaultClasses, " ").concat(classes).trim()); // concat classes merged to tailwind
+        return twMerge("".concat(defaultClasses, " ").concat(classes).trim()); // concat classes merged to tailwind
     };
 };
-exports.twStyle = twStyle;
